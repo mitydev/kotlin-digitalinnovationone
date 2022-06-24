0377d7c665ecf19a8335b133619d772d5a069ea5
mitydev <michael.tiago@hotmail.com>
2022-06-24 12:05:09 -0300

    Adicionado a classe Conta

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,5 +1,22 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
+    var nome: String = "Michael";
 
+    var cpf: String = "123.123.123-11"
+    private set;
+
+    constructor(){
+        println("Chamando construct");
+    }
+
+    fun info() = "$nome e $cpf";
+}
+
+fun main(){
+    val pessoa = Pessoa();
+
+    println(pessoa.nome);
+    println(pessoa.cpf);
+    println(pessoa.info());
 }
